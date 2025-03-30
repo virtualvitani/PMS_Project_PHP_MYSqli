334213d7547ac27dda6de93ab52dcc125510eb14
VirtualVitani <lukas.premuzi@gmail.com>
2025-03-30 10:46:33 +0200

    Upload

--- index.php
+++ index.php
@@ -1,63 +1,135 @@
+<?php
+
+$connection = mysqli_connect('localhost', 'virtualvitani', 'Elementa08', 'Reception_operations');
+    
+if($connection === false){
+    die("Connection failed: ". mysqli_connect_error());
+}
+
+$sql = "SELECT * from Arrivals";
+$result = mysqli_query($connection, $sql);
+
+if (mysqli_num_rows($result) === 0) {
+    die("There are no arrivals in our database!");
+}
+
+$arrivals = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reception Operations</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Connecting to Base</h1>
-
-    <?php
-        $connection = mysqli_connect('localhost', 'virtualvitani', 'Elementa08', 'Reception_operations');
-    
-        if($connection === false){
-            die("Connection failed: ". mysqli_connect_error());
-        }
-
-        $sql = "SELECT * from Arrivals";
-        $result = mysqli_query($connection, $sql);
-
-        if (mysqli_num_rows($result) > 0) {
-            $arrivals = mysqli_fetch_all($result);
-            var_dump($arrivals);die();
-            ?>
-
-            <table>
+    <h1>Arrivals</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Calendar Date</th>
+                <th>Reservation Number</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Room Type</th>
+                <th>Room</th>
+                <th>Guests</th>
+                <th>Departure Date</th>
+                <th>Agency</th>
+                <th>Note</th>
+                <th>Package</th>
+                <th>Price</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($arrivals as $arrival): ?>
                 <tr>
-                    <th>Calendar Date</th>
-                    <th>Reservation Number</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Room Type</th>
-                    <th>Room</th>
-                    <th>Guests</th>
-                    <th>Departure Date</th>
-                    <th>Agency</th>
-                    <th>Note</th>
-                    <th>Package</th>
-                    <th>Price</th>
+                    <td><?= $arrival['id'] ?></td>
+                    <td><?= $arrival['calendar_date'] ?></td>
+                    <td><?= $arrival['reservation_number'] ?></td>
+                    <td><?= $arrival['first_name'] ?></td>
+                    <td><?= $arrival['last_name'] ?></td>
+                    <td><?= $arrival['room_type'] ?></td>
+                    <td><?= $arrival['room'] ?></td>
+                    <td><?= $arrival['guests'] ?></td>
+                    <td><?= $arrival['departure_date'] ?></td>
+                    <td><?= $arrival['agency'] ?></td>
+                    <td><?= $arrival['note'] ?></td>
+                    <td><?= $arrival['package'] ?></td>
+                    <td><?= $arrival['price'] ?></td>
                 </tr>
-
-    <?php
-            foreach ($arrivals as $arrival) {
-                echo '<tr>';
-                echo '<td>' . $arrival[1] . '</td>';
-                echo '<td>' . $arrival[2] . '</td>';
-                echo '<td>' . $arrival[3] . '</td>';
-                echo '<td>' . $arrival[4] . '</td>';
-                echo '<td>' . $arrival[5] . '</td>';
-                echo '<td>' . $arrival[6] . '</td>';
-                echo '<td>' . $arrival[7] . '</td>';
-                echo '<td>' . $arrival[8] . '</td>';
-                echo '<td>' . $arrival[9] . '</td>';
-                echo '<td>' . $arrival[10] . '</td>';
-                echo '<td>' . $arrival[11] . '</td>';
-                echo '<td>' . $arrival[12] . '</td>';
-                echo '</tr>';
-            }
-        }
-    ?>
+            <?php endforeach ?>
+        </tbody>
     </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+</html>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container my-5">
+      <h1>Hello, world!</h1>
+      <div class="col-lg-8 px-0">
+        <p class="fs-5">You've successfully loaded up the Bootstrap starter example. It includes <a href="https://getbootstrap.com/">Bootstrap 5</a> via the <a href="https://www.jsdelivr.com/package/npm/bootstrap">jsDelivr CDN</a> and includes an additional CSS and JS file for your own code.</p>
+        <p>Feel free to download or copy-and-paste any parts of this example.</p>
+
+        <hr class="col-1 my-4">
+
+        <a href="https://getbootstrap.com" class="btn btn-primary">Read the Bootstrap docs</a>
+        <a href="https://github.com/twbs/examples" class="btn btn-secondary">View on GitHub</a>
+      </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="main.js"></script>
+  </body>
 </html>
